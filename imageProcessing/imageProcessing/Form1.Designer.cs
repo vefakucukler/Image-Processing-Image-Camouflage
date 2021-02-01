ea3069bd76e15b29ae51463f1398c2aa39724298
Vefa Küçükler <vefakucukler@hotmail.com>
2021-02-01 17:10:49 +0300

    Image-Processing-Image-Camouflage

---
+++ imageProcessing/imageProcessing/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace imageProcessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(29, 32);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(239, 244);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(64, 365);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 55);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Resim Yükle Ve Kamufle Et";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(627, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(239, 244);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(91, 294);
+            this.trackBar1.Minimum = 10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(109, 56);
+            this.trackBar1.TabIndex = 9;
+            this.trackBar1.Value = 10;
+            this.trackBar1.Visible = false;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Location = new System.Drawing.Point(356, 365);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(164, 55);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Kenarları Algıla";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox3.Location = new System.Drawing.Point(335, 32);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(239, 244);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(893, 457);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ imageProcessing/imageProcessing/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imageProcessing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int ResimKenari => trackBar1.Value;
+        private Bitmap KamufleEt(in Bitmap image)
+        {
+            Bitmap copyImage = image.Clone() as Bitmap;
+
+            List<Color> renkler = GetDifferentColorListInBitmap(copyImage, 10);
+            List<BitmapSatirDetail> KenarListesi = GetBorderCoordinates(copyImage);
+
+            RenkListesi(copyImage, KenarListesi, renkler);
+
+            return copyImage;
+        }
+        private void RenkListesi(Bitmap image, List<BitmapSatirDetail> KenarListesi, List<Color> colors)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < KenarListesi.Count; i++)
+            {
+                for (int j = KenarListesi[i].IlkKolon; j < KenarListesi[i].SonKolon; j++)
+                {
+                    var randomNumber = random.Next(0, colors.Count());
+
+                    image.SetPixel(i, j, colors[randomNumber]);
+                }
+            }
+        }
+        private List<BitmapSatirDetail> GetBorderCoordinates(Bitmap image)
+        {
+            List<BitmapSatirDetail> KenarListesi = new List<BitmapSatirDetail>();
+
+            for (int i = 1; i < image.Width - 1; i++)
+            {
+                BitmapSatirDetail SatirDetail = new BitmapSatirDetail();
+
+                SatirDetail.Satir = i;
+
+                for (int j = 1; j < image.Height - 1; j++)
+                {
+                    Color cr = image.GetPixel(i + 1, j);
+                    Color cl = image.GetPixel(i - 1, j);
+                    Color cu = image.GetPixel(i, j - 1);
+                    Color cd = image.GetPixel(i, j + 1);
+                    int dx = cr.R - cl.R;
+                    int dy = cd.R - cu.R;
+                    double formul = Math.Sqrt(dx * dx / 4 + dy * dy / 4);
+
+                    if (formul > ResimKenari)
+                    {
+                        if (SatirDetail.IlkKolon == 0)
+                        {
+                            SatirDetail.IlkKolon = j;
+                        }
+                        else
+                        {
+                            SatirDetail.SonKolon = j;
+                        }
+                    }
+                }
+
+                KenarListesi.Add(SatirDetail);
+            }
+
+            return KenarListesi;
+        }
+        private List<Color> GetDifferentColorListInBitmap(Bitmap image, int count)
+        {
+            List<Color> colors = new List<Color>();
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    var color = image.GetPixel(x, y);
+                    colors.Add(color);
+                }
+            }
+            return colors
+                .GroupBy(i => i)
+                .Select(group => new
+                {
+                    Key = group.Key,
+                    Count = group.Count()
+                })
+                .OrderByDescending(i => i.Count)
+                .Take(count)
+                .Select(i => i.Key)
+                .ToList();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        private Bitmap BinaryYap(Bitmap image)
+        {
+            Bitmap gri = griYap(image);
+            int temp = 0;
+            int esik = 155;
+            Color renk;
+            for (int i = 0; i < gri.Height - 1; i++)
+            {
+                for (int j = 0; j < gri.Width - 1; j++)
+                {
+                    temp = gri.GetPixel(j, i).R;
+
+                    if (temp < esik)
+                    {
+                        renk = Color.FromArgb(0, 0, 0);
+                        gri.SetPixel(j, i, renk);
+                    }
+                    else
+                    {
+                        renk = Color.FromArgb(255, 255, 255);
+                        gri.SetPixel(j, i, renk);
+                    }
+                }
+            }
+            return gri;
+        }
+        private Bitmap griYap(Bitmap bmp)
+        {
+            for (int i = 0; i < bmp.Height - 1; i++)
+            {
+                for (int j = 0; j < bmp.Width - 1; j++)
+                {
+                    int deger = (bmp.GetPixel(j, i).R + bmp.GetPixel(j, i).G + bmp.GetPixel(j, i).B) / 3;
+
+                    Color renk;
+                    renk = Color.FromArgb(deger, deger, deger);
+                    bmp.SetPixel(j, i, renk);
+                }
+            }
+            return bmp;
+        }    
+        OpenFileDialog file = new OpenFileDialog();
+        private void button2_Click(object sender, EventArgs e)
+        {          
+            var dialogResult = file.ShowDialog();
+           
+            Bitmap image = new Bitmap(file.FileName);
+            pictureBox1.Image = image;
+            Bitmap newMaskAppliedImage = KamufleEt(in image);
+            pictureBox2.Image = newMaskAppliedImage;
+        }
+        class BitmapSatirDetail
+        {
+            public int Satir { get; set; }
+            public int IlkKolon { get; set; }
+            public int SonKolon { get; set; }
+        }
+        private Bitmap sobelEdgeDetection(Bitmap image)
+        {
+            Bitmap gri = griYap(image);
+            Bitmap buffer = new Bitmap(gri.Width, gri.Height);
+            Color renk;
+            int valX, valY, gradient;
+            int[,] GX = new int[3, 3];
+            int[,] GY = new int[3, 3];
+
+            //Yatay yönde kenar
+            GX[0, 0] = -1; GX[0, 1] = 0; GX[0, 2] = 1;
+            GX[1, 0] = -2; GX[1, 1] = 0; GX[1, 2] = 2;
+            GX[2, 0] = -1; GX[2, 1] = 0; GX[2, 2] = 1;
+
+            //Düşey yönde kenar
+            GY[0, 0] = -1; GY[0, 1] = -2; GY[0, 2] = -1;
+            GY[1, 0] = 0; GY[1, 1] = 0; GY[1, 2] = 0;
+            GY[2, 0] = 1; GY[2, 1] = 2; GY[2, 2] = 1;
+
+
+            for (int i = 0; i < gri.Height; i++)
+            {
+                for (int j = 0; j < gri.Width; j++)
+                {
+                    if(i==0 || i==gri.Height-1 || j==0 || j == gri.Width - 1)
+                    {
+                        renk = Color.FromArgb(155, 155, 150);
+                        buffer.SetPixel(j, i, renk);
+                        valX = 0;
+                        valY = 0;                           
+                    }
+                    else
+                    {
+                        valX = gri.GetPixel(j - 1, i - 1).R * GX[0, 0] +
+                            gri.GetPixel(j, i-1).R * GX[0, 1] +
+                            gri.GetPixel(j + 1, i - 1).R * GX[0, 2] +
+                            gri.GetPixel(j - 1, i).R * GX[1, 0] +
+                            gri.GetPixel(j, i).R * GX[1, 1] +
+                            gri.GetPixel(j +1 , i).R * GX[1, 2] +
+                            gri.GetPixel(j - 1, i + 1).R * GX[2, 0] +
+                            gri.GetPixel(j, i +1).R * GX[2, 1] +
+                            gri.GetPixel(j + 1, i + 1).R * GX[2, 2];
+
+                        valY = gri.GetPixel(j - 1, i - 1).R * GY[0, 0] +
+                           gri.GetPixel(j, i - 1).R * GY[0, 1] +
+                           gri.GetPixel(j+1, i - 1).R * GY[0, 2] +
+                           gri.GetPixel(j - 1, i).R * GY[1, 0] +
+                           gri.GetPixel(j, i).R * GY[1, 1] +
+                           gri.GetPixel(j + 1, i).R * GY[1, 2] +
+                           gri.GetPixel(j - 1, i + 1).R * GY[2, 0] +
+                           gri.GetPixel(j, i + 1).R * GY[2, 1] +
+                           gri.GetPixel(j + 1, i + 1).R * GY[2, 2];
+
+                        gradient = (int)(Math.Abs(valX) + Math.Abs(valY));
+
+                        if (gradient < 0)
+                        {
+                            gradient = 0;
+                        }
+                        if (gradient > 255)
+                        {
+                            gradient = 255;
+                        }
+
+                        renk = Color.FromArgb(gradient, gradient, gradient);
+                        buffer.SetPixel(j, i, renk);
+                    }
+                }
+            }
+            return buffer;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {           
+            Bitmap image = new Bitmap(pictureBox1.Image);
+            Bitmap sobel = sobelEdgeDetection(image);
+            pictureBox3.Image = sobel;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
